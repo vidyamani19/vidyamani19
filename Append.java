998f5ff4ea0a201ac5011a4252340da74f373b37
vidyamani19 <40511628+vidyamani19@users.noreply.github.com>
2018-06-26 11:15:46 +0000

    Create Append.java

---
+++ Append.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Append
+{
+    public static void main(String args[])
+    {
+        Scanner s = new Scanner(System.in);
+        String s1 = s.nextLine();
+        System.out.print(s1+".");
+    }
+}
